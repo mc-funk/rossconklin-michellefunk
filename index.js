ec6537ef262f65fe3554f74185c2aa33a2072ee8
Michelle C. Funk <michelle@michellefunk.com>
2016-01-17 13:41:30 -0600

    Integrate bootstrap template

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var path = require("path");
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -7,14 +8,12 @@ app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/*', function(request, response) {
+    var file = request.params[0] || 'pages/index.html';
+    response.sendFile(path.join(__dirname, './views', file));
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
