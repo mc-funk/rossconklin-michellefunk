d20fe1a7c21279bc49d16e7ea039c1775e88b64c
Michelle C. Funk <michelle@michellefunk.com>
2016-01-17 16:42:09 -0600

    RSVP tweaks

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -32,3 +32,10 @@
     });
 
 })(jQuery);
+
+//from http://www.sanwebe.com/2014/08/css-html-forms-designs
+//auto expand textarea
+function adjust_textarea(h) {
+    h.style.height = "20px";
+    h.style.height = (h.scrollHeight)+"px";
+}
