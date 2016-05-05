92ab8836cfea5ab6dd4f66f89e6d17445ea48022
Michelle C. Funk <michelle@michellefunk.com>
2016-05-04 20:53:49 -0500

    Add google tracking

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -82,6 +82,14 @@
         '<div class="panel-body"><p>' + faq[i]["answer"] + ' </p></div></div></div>'
       );
     }
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||
+    function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();
+    a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;
+    m.parentNode.insertBefore(a,m)})
+    (window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-72472893-2', 'auto');
+    ga('send', 'pageview');
+
 })(jQuery);
 
 //from http://www.sanwebe.com/2014/08/css-html-forms-designs
